74126fd92d43b0418420eb15d9070220d15f98af
Stepan Kononov <kononov_stepan@mail.ru>
2023-03-29 16:09:02 +0800

    Список формируется

--- src/pages/teams/TeamsPage.tsx
+++ src/pages/teams/TeamsPage.tsx
@@ -1,13 +1,42 @@
-import { Box, Typography } from "@mui/material";
-import { useGetUserDataQuery } from "../../services/user";
+import { Box, Typography, Container } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { User } from "../user/User";
 
-export const Teams = () => {
-  const { data } = useGetUserDataQuery();
-  console.log(data);
+import { useGetUserDataQuery } from "../../services/user"
 
-  return (
+export const Teams = () => { 
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetUserDataQuery()
+
+let listUsers
+const urlImg = "https://cs13.pikabu.ru/avatars/658/x658267-1013849002.png"
+
+if (isLoading) {
+  listUsers = <div>Loading...</div>
+} else if (isSuccess) {
+  listUsers = users.map((user) => {return (<User urlImg={urlImg} name={user.name} company={user.company.name} />)});
+} else if (isError) {
+  listUsers = <div>error...</div>
+}
+
+const MyContainer = styled(Container)({
+  background: '#EAEAEA',  
+  padding: '24px',
+  maxWidth: '370px',
+});
+
+
+return (
     <Box>
-      <Typography variant="h1">Команды</Typography>
+      <Typography variant="h1">Команды</Typography>    
+      <MyContainer>            
+        {listUsers}                  
+      </MyContainer>
     </Box>
   );
 };
